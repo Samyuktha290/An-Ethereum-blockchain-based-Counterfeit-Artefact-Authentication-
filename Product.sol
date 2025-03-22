87f405fba1f0e4f8ef3914809bdc6aaed34604a2
Samyuktha290 <137173752+Samyuktha290@users.noreply.github.com>
2025-03-22 14:52:45 +0530

    Add files via upload

---
+++ Product.sol
@@ -0,0 +1,48 @@
+pragma solidity >= 0.8.11 <= 0.8.21;
+
+contract Product {
+    string public users;
+    string public product;
+    string public order;
+    string public history;
+
+    function addUsers(string memory u) public {
+        users = u;	
+    }
+
+    function getUsers() public view returns (string memory) {
+        return users;
+    }
+
+    function addproduct(string memory ba) public {
+        product = ba;	
+    }
+
+    function getproduct() public view returns (string memory) {
+        return product;
+    }
+
+    function addorder(string memory ca) public {
+        order = ca;
+    }
+
+    function getorder() public view returns (string memory) {
+        return order;
+    }
+
+    function addhistory(string memory ra) public {
+        history = ra;
+    }
+
+    function gethistory() public view returns (string memory) {
+        return history;
+    }
+
+    constructor() public {
+        users = "empty";
+	product = "empty";
+    order = "empty";
+    history = "empty";
+    
+    }
+}
